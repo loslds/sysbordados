1e39dc85e918c63aa31fb723befcd974c0b3d952
loslds <los.sbrissa@gmail.com>
2022-04-12 20:35:05 -0300

    iniciando pg login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Avatar, Grid, Paper } from '@material-ui/core'
+import { LockOutlined} from '@material-ui/icons'
+
 
 export const PageLogin = () => {
+
+  const paperStyle= {
+    padding: 20,
+    heigth: '330vh',
+    width: 280,
+    margin:'20px auto',
+    align: 'center'
+  }
+  const avatarStyle={
+    backgroundColor: '#fb0606'
+  }
   return (
-    <h1>PageLogin</h1>
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align ='center'>
+          <Avatar style={avatarStyle}><LockOutlined /></Avatar>
+          <h2>Identifique-se.</h2>
+        </Grid>
+      </Paper>
+    </Grid>
+
   )
 }
